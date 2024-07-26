5017c527dc170ca72a1d599fb7ef7a90483a3fb9
mekachi015 <abielmakoti@gmail.com>
2024-07-26 12:59:58 +0200

    First commit

---
+++ ScoreApp-Backend/src/main/java/com/example/ScoreApp/ScoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScoreApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreAppApplication.class, args);
+	}
+
+}
---
+++ ScoreApp-Backend/src/test/java/com/example/ScoreApp/ScoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
